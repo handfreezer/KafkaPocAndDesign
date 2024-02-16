2fb3f9a7babc7a1bf3a6cda70fe00fc194f8651b
handfreezer <laplagne@ulukai.net>
2024-02-17 00:46:25 +0100

    Adding a step to test POC : building image only once (it is not a space problem, but a clean view in docker image, and later for generating TLS files for POC... :-) )

---
+++ 010-build-image-kfk-poc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BUILD_DATE=$(date +%Y%m%d-%H%M%S)
+
+docker build -t kfk:${BUILD_DATE} -f image-kfk/kraft.dockerfile image-kfk
+res=$?
+if [ 0 -ne "${res}" ]
+then
+	echo "Failed to build image-kfk, this should work first!"
+else
+	echo "IMG_KFK=kfk:${BUILD_DATE}" >.env
+fi
+
--- 999-reset-poc.sh
+++ 999-reset-poc.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm -f .env
 docker compose stop
 docker compose rm -f
+docker image ls -f 'reference=kfk' --format '{{.ID}}'|sort|uniq|xargs docker image rm -f
+#docker system prune => shoud not be a good idea for everyone in POC environment
 
