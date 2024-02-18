fd20073aaa6955099d02e229d385e7e92b73e933
handfreezer <laplagne@ulukai.net>
2024-02-18 01:25:01 +0100

    Cleaning before real start of job

--- 999-reset-poc.sh
+++ 999-reset-poc.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-rm -f .env
 docker compose stop
 docker compose rm -f
+
 docker image ls -f 'reference=kfk' --format '{{.ID}}'|sort|uniq|xargs docker image rm -f
+rm -f .env
+
 #docker system prune => shoud not be a good idea for everyone in POC environment
+
+find datas-poc/connect/{smt,libs} -name '*.jar' -exec rm -f {} \;
+find datas-poc/ -name 'kraft.*' -exec rm {} \;
 
